ad70045c2a1b2357af87be1a10f88d929ce9aacc
breakTheFor1 <thouvouh@turoid.com>
2016-07-15 16:38:26 +0300

    Update SkyBlock.php

--- src/SkyBlock/SkyBlock.php
+++ src/SkyBlock/SkyBlock.php
@@ -3,7 +3,20 @@
 namespace SkyBlock;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class SkyBlock extends PluginBase{
+ public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+  if($cmd->getName() == "ada"){
+   if(!empty($args[0])){
+    if(strtolower($args[0]) == "olustur"){
+    }
+    else if(strtolower($args[0]) == "isinlan"){
+    }
+   }
+  }
+ }
 }
 ?>
